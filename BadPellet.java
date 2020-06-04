e107dcc23ae902e117e9093457a6c4fcb44ce8d1
rkosuri21 <43041726+rkosuri21@users.noreply.github.com>
2020-06-04 18:49:59 -0400

    Add files via upload

--- BadPellet.java
+++ BadPellet.java
@@ -1,29 +1,27 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.geom.*;
+import java.awt.Rectangle;
 
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
-public class Pellet extends JComponent
+
+public class BadPellet extends JComponent
 {
 	
-	public Pellet(int x, int y) 
+	public BadPellet(int x, int y) 
 	{
-		this.setSize(new Dimension(21,41));
+		this.setSize(new Dimension(25,45));
 		this.setLocation(x,y);
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.setColor(Color.RED);
+		g.setColor(new Color(99, 15, 255,255));
 		g.fillOval(0, 20, 20, 17);
-		g.setColor(Color.black);
+		g.setColor(Color.BLACK);
 		g.fillRect(10, 15, 3, 7);
-		g.setColor(Color.green);
+		g.setColor(Color.DARK_GRAY);
 		g.fillRect(5, 13, 5, 3);
 	}
 
--- Main.java
+++ Main.java
@@ -1,66 +1,119 @@
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Main extends JFrame implements ActionListener
-{
-	private Snake snek = new Snake((int)(Math.random()*900), (int)(Math.random()*600));
+{ 
+	private Snake snek = new Snake(450,300);
+	private Rectangle borderSnek = new Rectangle (450,300,15,15);
 	private Pellet ball = new Pellet((int)(Math.random()*800), (int)(Math.random()*500));
+	private BadPellet bad = new BadPellet((int)(Math.random()*800), (int)(Math.random()*500));
+	private Rectangle borderBad = new Rectangle (bad.getX(), bad.getY(),21,41);
+	private Rectangle borderBall = new Rectangle (ball.getX(), ball.getY(),21,41);
 	
-	private JPanel p = new JPanel();
-	private Insets insets = new Insets(0,0,70,0);
+	private boolean gameOver = false;
+	private int scNum = 0;
+	private JLabel gameEnd = new JLabel("");
+	JLabel score = new JLabel(scNum + "");
+
 	public Main() 
 	{
-		setTitle("Snake: The Game");
-		setLayout(null);
-		setBounds(0,0,900,600);
-		setResizable(false);
+		this.setTitle("Snake: The Game");
+		this.setBounds(0,0,900,600);
+		this.setResizable(false);
+		this.setPreferredSize(new Dimension(900,600));
+		this.pack();
+		this.setLayout(null);
+		
+		JPanel panel2 = new JPanel();
+		panel2.setSize(new Dimension (900,600));
+		
+		JLabel inst = new JLabel("USE THE WASD KEYS TO MOVE THE SNAKE!");
+		inst.setFont(new Font("SERIF", Font.BOLD, 15));
+		inst.setForeground(Color.WHITE);
+		inst.setSize(400,40);
+		panel2.add(inst);
+		
+		JLabel inst2 = new JLabel("DONT EAT THE WICKED PURPLE APPLE!");
+		inst2.setFont(new Font("SERIF", Font.BOLD, 15));
+		inst2.setForeground(Color.WHITE);
+		inst2.setSize(400,40);
+		panel2.add(inst2);
+
+		this.gameEnd.setBounds(new Rectangle(250,50,900,50));
+		this.gameEnd.setLocation(300, 250);
+		this.gameEnd.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+		this.gameEnd.setForeground(Color.WHITE);
+		this.add(this.gameEnd);
+		
+		score.setBounds(new Rectangle(50,30,100,30));
+		score.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		score.setForeground(Color.YELLOW);
+		add(score);
 		
 		this.addKeyListener(new KeyListener()
 		{
-
-			@Override
 			public void keyPressed(KeyEvent e) 
 			{
 				if(e.getKeyCode() == e.VK_W)
-					snek.setDy(-1);
+				{
+					snek.setDy(-2);
+					snek.setDx(0);
+				}
 				if(e.getKeyCode() == e.VK_S)
-					snek.setDy(1);
-					
-				if(e.getKeyCode() == e.VK_UP)
-					snek.setDy(-1);
-				if(e.getKeyCode() == e.VK_DOWN)
-					snek.setDy(1);
-					
+				{
+					snek.setDy(2);
+					snek.setDx(0);
+				}
+				if(e.getKeyCode() == e.VK_A)
+				{
+					snek.setDx(-2);
+					snek.setDy(0);
+				}
+				if(e.getKeyCode() == e.VK_D)
+				{
+					snek.setDx(2);
+					snek.setDy(0);
+				}
+				inst.setText("");
+				inst2.setText("");
 			}
-
-			@Override
+			public void keyTyped(KeyEvent e) 
+			{
+			}
 			public void keyReleased(KeyEvent e) 
 			{
-//				if(e.getKeyCode() == e.VK_W)
-//					snek.setDy(0);
-//				if(e.getKeyCode() == e.VK_S)
-//					snek.setDy(0);
-//					
-//				if(e.getKeyCode() == e.VK_UP)
-//					snek.setDy(0);
-//				if(e.getKeyCode() == e.VK_DOWN)
-//					snek.setDy(0);
-				
 			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-	
 		});
+		
+		this.add(snek);
+		this.add(ball);
+		this.add(bad);
+		
+		panel2.setBackground(new Color(0,0,77,255));
+		this.add(panel2);
+		
+		Timer t1 = new Timer(10,this);
+		t1.start();
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) 
@@ -70,15 +123,55 @@ public class Main extends JFrame implements ActionListener
 
 	public void actionPerformed(ActionEvent e) 
 	{
-		if(snek.getY() < 0)
-			snek.setDy(1);
-		if(snek.getX() < 0)
-			snek.setDy(-1);
-		if(ball.getY() + ball.getHeight() > this.getContentPane().getHeight())
-			snek.setDy(-1);
-		if(ball.getX() + ball.getWidth() > this.getContentPane().getWidth())
-			snek.setDy(1);
+		snek.update();
+		borderSnek.setLocation(snek.getX(), snek.getY());
 		
+		if(snek.getX() >= 850)
+			snek.setLocation(10, snek.getY());
+		if(snek.getX() <= 1)
+			snek.setLocation(899, snek.getY());
+		if(snek.getY() >= 550)
+			snek.setLocation(snek.getX(), 10);
+		if(snek.getY() <= 1)
+			snek.setLocation(snek.getX(), 599);
+		
+		if (borderSnek.intersects(borderBall))
+		{
+			ball.setLocation((int)(Math.random()*800), (int)(Math.random()*500));
+			borderBall.setLocation(ball.getX(), ball.getY());
+			scNum++;
+			score.setText((scNum) + "");
+			bad.setLocation((int)(Math.random()*800), (int)(Math.random()*500));
+			borderBad.setLocation(bad.getX(), bad.getY());
+			while(borderBad.intersects(borderBall))
+			{
+				bad.setLocation((int)(Math.random()*800), (int)(Math.random()*500));
+				borderBad.setLocation(bad.getX(), bad.getY());
+			}
+			if (scNum >= 10)
+			{
+				snek.setDx(0);
+				snek.setDy(0);
+				remove(ball);
+				remove(snek);
+				gameEnd.setText("YOU WON!!!");
+				System.out.print("\uD83C\uDF88");//its supposed to print a balloon emoji in the console but it didnt work on my computer, I hope it works on yours :)
+				System.out.print("\uD83E\uDD51"); //its supposed to print avocado emoji in the console but it didnt work on my computer, I hope it works on yours :)
+			}
+		}
+		
+		if(borderSnek.intersects(borderBad))
+			gameOver = true;
+		
+		if(gameOver)
+		{
+			snek.setDx(0);
+			snek.setDy(0);
+			remove(ball);
+			remove(snek);
+			gameEnd.setText("GAME OVER");
+			return;
+		}
+		repaint();
 	}
-
 }
